327e47c8a0e9379a99d6ed42e44603c748cc6927
Jordi Casanella <jcasanella@mimecast.com>
2021-10-20 00:26:40 +0100

    First tests with k8s

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"path/filepath"
 
 	"github.com/jcasanella/k8s_dashboard/router"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
+
+	// Check kubernetes configs
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	_, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	router := router.Router()
 
 	error := router.Run("localhost:8085")
