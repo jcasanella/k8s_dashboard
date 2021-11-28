123f7412c4699c933444813dc5c688754d1f976d
jcasanella <jordikiter@hotmail.es>
2021-11-28 23:49:36 +0000

    Drop albums object

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,73 +1,73 @@
-async function fetchAlbumsJSON() {
-    const response = await fetch("http://localhost:8085/v1/albums");
+// async function fetchAlbumsJSON() {
+    // const response = await fetch("http://localhost:8085/v1/albums");
 
-    if (!response.ok) {
-        const message = 'An error has occured: ${response.status}';
-        throw new Error(message);
-    }
+    // if (!response.ok) {
+    //     const message = 'An error has occured: ${response.status}';
+    //     throw new Error(message);
+    // }
 
-    const albums = await response.json();
-    return albums;
-}
+    // const albums = await response.json();
+    // return albums;
+// }
 
-function getAlbums() {
-    fetchAlbumsJSON().then(data => {
-        if (Object.keys(data).length > 0) {
+// function getAlbums() {
+//     fetchAlbumsJSON().then(data => {
+//         if (Object.keys(data).length > 0) {
 
-            var table_json = document.getElementById("table_json");
-            var table = document.createElement("table");
-            table.classList.add("table", "table-striped", "table-hover");
+//             var table_json = document.getElementById("table_json");
+//             var table = document.createElement("table");
+//             table.classList.add("table", "table-striped", "table-hover");
 
-            createHeader(table);
+//             createHeader(table);
 
-            var tBody = document.createElement("tbody");
-            for (i = 0; i < data.length; i++) {
-                createRow(data[i], tBody);
-            }
-            table.appendChild(tBody);
-            table_json.appendChild(table);
-        }
-        console.log(data);
-    }).catch(error => {
-        alert(error.message);
-    });
-}
+//             var tBody = document.createElement("tbody");
+//             for (i = 0; i < data.length; i++) {
+//                 createRow(data[i], tBody);
+//             }
+//             table.appendChild(tBody);
+//             table_json.appendChild(table);
+//         }
+//         console.log(data);
+//     }).catch(error => {
+//         alert(error.message);
+//     });
+// }
 
-function createHeader(table) {
-    var tHead = document.createElement("thead");
-    var row = document.createElement("tr");
+// function createHeader(table) {
+//     var tHead = document.createElement("thead");
+//     var row = document.createElement("tr");
 
-    createHeaderCell(row, "Id");
-    createHeaderCell(row, "Name");
-    createHeaderCell(row, "Title");
-    createHeaderCell(row, "Price");
+//     createHeaderCell(row, "Id");
+//     createHeaderCell(row, "Name");
+//     createHeaderCell(row, "Title");
+//     createHeaderCell(row, "Price");
 
-    tHead.appendChild(row);
-    table.appendChild(tHead);
-}
+//     tHead.appendChild(row);
+//     table.appendChild(tHead);
+// }
 
-function createHeaderCell(row, text) {
-    var th = document.createElement("th");
-    th.setAttribute("scope", "col");
-    var cell = document.createTextNode(text);
-    th.appendChild(cell);
-    row.appendChild(th);
-}
+// function createHeaderCell(row, text) {
+//     var th = document.createElement("th");
+//     th.setAttribute("scope", "col");
+//     var cell = document.createTextNode(text);
+//     th.appendChild(cell);
+//     row.appendChild(th);
+// }
 
-function createRow(text, table) {
-    var row = document.createElement("tr");
+// function createRow(text, table) {
+//     var row = document.createElement("tr");
 
-    createCell(text.id, row);
-    createCell(text.artist, row);
-    createCell(text.title, row);
-    createCell(text.price, row);
+//     createCell(text.id, row);
+//     createCell(text.artist, row);
+//     createCell(text.title, row);
+//     createCell(text.price, row);
 
-    table.appendChild(row);
-}
+//     table.appendChild(row);
+// }
 
-function createCell(text, row) {
-    var cell = document.createElement("td");
-    var textCell = document.createTextNode(text);
-    cell.appendChild(textCell);
-    row.appendChild(cell);
-}
+// function createCell(text, row) {
+//     var cell = document.createElement("td");
+//     var textCell = document.createTextNode(text);
+//     cell.appendChild(textCell);
+//     row.appendChild(cell);
+// }
--- router/router.go
+++ router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/jcasanella/k8s_dashboard/middleware"
 	repository "github.com/jcasanella/k8s_dashboard/repository"
 )
 
@@ -11,18 +10,6 @@ func Router() *gin.Engine {
 
 	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("templates/*.tmpl")
-
-	v1 := router.Group("/v1")
-	{
-		v1.GET("/albums", middleware.GetAlbums)
-		v1.GET("/albums/:id", middleware.GetAlbumByID)
-		v1.POST("/albums", middleware.PostAlbums)
-	}
-
-	v2 := router.Group("/v2")
-	{
-		v2.GET("/albums", middleware.GetTitle)
-	}
 
 	k8s := router.Group("/k8s")
 	{
