6ea040ba9aaf84e08d8d88bc1bebe62cbe21e86d
jcasanella <jordikiter@hotmail.es>
2021-12-06 00:37:51 +0000

    client url js

---
+++ assets/js/client.js
@@ -0,0 +1,16 @@
+class Client {
+    static async getResponse(url) {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('An error has occurred: ${response.status} ')
+        }
+
+        return await response.json();
+    }
+}
+
+function getNamespace() {
+    Client.getResponse('http://localhost:8085/v1/k8s/namespaces')
+        .then(console.log());
+}
