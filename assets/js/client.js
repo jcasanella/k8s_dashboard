25b68ac5d2e4d3e7440e02ba5902f7362a1c7d14
Jordi Casanella <jcasanella@mimecast.com>
2021-12-24 00:15:31 +0100

    day2: Javascript to process the Json from backend

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,8 +1,9 @@
 "use strict";
 
 class Client {
-    static async getResponse(url) {
-        const response = await fetch(url)
+    static async getResponse(endpoint, apiVersion = 'v1', apiType = 'k8s') {
+        let serverAddress = 'http://localhost:8085';
+        const response = await fetch(serverAddress + '/' + apiVersion + '/' + apiType + '/' + endpoint)
 
         if (!response.ok) {
             throw new Error('An error has occurred: ${response.status} ')
@@ -13,6 +14,32 @@ class Client {
 }
 
 function getNamespace() {
-    Client.getResponse('http://localhost:8085/v1/k8s/namespaces')
+    Client.getResponse('namespaces')
+        .then(processJson);
+}
+
+function getPods() {
+    Client.getResponse('pods')
+        .then(processJson);
+}
+
+function getConfigMaps() {
+    Client.getResponse('configmaps')
+        .then(processJson);
+}
+
+function countPods() {
+    Client.getResponse('pods/count')
         .then(console.log);
+}
+
+function countConfigMaps() {
+    Client.getResponse('configmaps/count')
+        .then(console.log);
+}
+
+function processJson(value) {
+    const jsonString = JSON.stringify(value);
+    const jsonObj = JSON.parse(jsonString);
+    Array.from(jsonObj).forEach(element => console.log(element));
 }
